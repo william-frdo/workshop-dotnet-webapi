09a481b01d18cd35daca3b711ae36e32cd797df8
William Florido <william.frdo@gmail.com>
2023-07-13 20:07:59 -0300

    API Web com banco de dados postgreSQL

---
+++ ContosoPizza-BD/ContosoPizza/Program.cs
@@ -0,0 +1,39 @@
+using ContosoPizza.Data;
+using ContosoPizza.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add the PizzaContext
+var postgreSQLConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<PizzaContext>(options =>
+    options.UseNpgsql(postgreSQLConnection));
+
+// Add the PromotionsContext
+
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
+
+app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
+
+app.Run();
